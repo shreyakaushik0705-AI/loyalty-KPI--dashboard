fbd98e8bbed5735ad991a9c498d04428223a6d20
shreyakaushik0705-AI <shreyakaushik0705@gmail.com>
2025-08-16 08:45:11 +0530

    Add files via upload

---
+++ kpis.py
@@ -0,0 +1,7 @@
+def calculate_kpi(data):
+    
+    return {"CTR": 0.05, "Conversion_rate": 0.02}
+
+if __name__ == "__main__":
+    dummy_data = {"campaign_id": 1}
+    print(calculate_kpi(dummy_data))
